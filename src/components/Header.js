018b495032227e823a66cd2f287c36ea4ba8a32f
Raiff Smith Said <raiff.said@gmail.com>
2020-05-19 23:59:28 -0300

    Added Style Loader and CSS Loader configurations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-/**
- * Para usar JS dentro do HTML, basta usar {} envolta da variável
- * A variável children é nativa do ReactJS, ela busca
- * os elementos inseridos dentro das tags
- * */ 
 export default function Header({ title }) {
   return (
     <header>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './App'; // Importando o componente App
+import App from './App'; 
 
-// Abre a tag e fecha nela mesma ao chamar um componente
 render(<App />, document.getElementById('app')); 
